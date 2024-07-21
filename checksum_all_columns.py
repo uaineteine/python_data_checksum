b002bbce67fac1119811816fe2d4c1058c3d9588
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-07-21 17:58:46 +1000

    fixed service

--- checksum_all_columns.py
+++ checksum_all_columns.py
@@ -23,40 +23,32 @@ def checksum_all_columns(file1, file2, file_type1='csv', file_type2='csv'):
     missing_in_df1 = set(df2.columns) - set(df1.columns)
     missing_in_df2 = set(df1.columns) - set(df2.columns)
     
-    # Compute MD5 checksums for the common columns and find matches
+    # Compute checksums for the common columns
     matches = []
+    non_matches = []
     for column in common_columns:
-        df1['checksum'] = df1[column].apply(lambda x: hashlib.md5(str(x).encode()).hexdigest())
-        df2['checksum'] = df2[column].apply(lambda x: hashlib.md5(str(x).encode()).hexdigest())
-        match = df1[df1['checksum'].isin(df2['checksum'])][[column, 'checksum']]
-        match['column'] = column
-        matches.append(match)
-    
-    # Concatenate all matches into a single DataFrame
-    matches_df = pd.concat(matches, ignore_index=True)
-    
-    # Create DataFrames for missing columns
-    missing_in_df1_df = pd.DataFrame(list(missing_in_df1), columns=['missing_in_df1'])
-    missing_in_df2_df = pd.DataFrame(list(missing_in_df2), columns=['missing_in_df2'])
-    
-    return matches_df, missing_in_df1_df, missing_in_df2_df
+        checksum1 = hashlib.md5(df1[column].astype(str).sum().encode()).hexdigest()
+        checksum2 = hashlib.md5(df2[column].astype(str).sum().encode()).hexdigest()
+        if checksum1 == checksum2:
+            matches.append({'column': column, 'checksum': checksum1})
+        else:
+            non_matches.append({'column': column, 'checksum': checksum1})
+    return pd.DataFrame(matches), pd.DataFrame(non_matches)
 
 def process_dataframes(file1, file2, file_type1, file_type2):
     # Call the checksum_all_columns function
-    matches_df, missing_in_df1_df, missing_in_df2_df = checksum_all_columns(file1, file2, file_type1, file_type2)
-    
+    matches_df, non_matching_df = checksum_all_columns(file1, file2, file_type1, file_type2)
+
     # Add 'status' column
     matches_df['status'] = 'match'
-    missing_in_df1_df['status'] = 'missing_in_df1'
-    missing_in_df2_df['status'] = 'missing_in_df2'
+    non_matching_df['status'] = 'nonmatch'
     
     # Rename columns to avoid conflicts
     matches_df = matches_df.rename(columns={'column': 'variable'})
-    missing_in_df1_df = missing_in_df1_df.rename(columns={'missing_in_df1': 'variable'})
-    missing_in_df2_df = missing_in_df2_df.rename(columns={'missing_in_df2': 'variable'})
+    non_matching_df = non_matching_df.rename(columns={'column': 'variable'})
     
     # Append all DataFrames
-    final_df = pd.concat([matches_df, missing_in_df1_df, missing_in_df2_df], ignore_index=True)
+    final_df = pd.concat([matches_df, non_matching_df], ignore_index=True)
     
     return final_df
 
